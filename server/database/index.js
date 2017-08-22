b0c70222db722d44b5483e01d7e543c035d41a8d
Terry Yun <nooheat1228@gmail.com>
2017-08-22 20:29:44 +0900

    [Done] Database connection module

--- server/database/index.js
+++ server/database/index.js
@@ -2,10 +2,10 @@ let mongoose = require('mongoose');
 
 module.exports = () => {
     mongoose.Promise = global.Promise;
+    // process.env는 환경변수에 접근함.
     mongoose.connect(process.env.TELLIN_DB_URL);
     mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error.'));
     mongoose.connection.on('open', function () {
-        // process.env는 환경변수에 접근함.
         console.log(`Connected to database - ${mongoose.connection.db.databaseName}`);
     });
     mongoose.connection.on('disconnected', () => {
