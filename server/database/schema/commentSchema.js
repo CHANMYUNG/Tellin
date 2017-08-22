cc9dc7a5f599674b01806965eada062b64b448eb
syeutyu <syeuty@naver.com>
2017-08-22 22:32:20 +0900

    veta schema

---
+++ server/database/schema/commentSchema.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let schema = mongoose.Schema({
+    like_count: { type: Number, default: null },
+    comment_date: { type: Date, default: Date.now() },
+    comment_body: { type: String, require: true }
+}, { collection: 'CommetnData' });
+
+/* 
+    like_count : 공감수
+    comment_date : 댓글 작성일
+    comment_body : 댓글 작성 내용
+*/
+module.exports = schema;
