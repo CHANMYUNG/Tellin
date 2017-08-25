a8a387c724d514f600280a6dc98655f38c774a19
Terry Yun <nooheat1228@gmail.com>
2017-08-25 22:52:38 +0900

    [DONE] router attached

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const database = require('./database');
 const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.set('port', 8080);
 
 // process.env는 환경변수에 접근함.
 app.set('jwt-secret', process.env.TELLIN_JWT_SECRET);
+
+app.use('/', router);
 
 app.listen(app.get('port'), () => {
     console.log(`NOW, SERVER IS RUNNING ON ${app.get('port')} PORT.`);
