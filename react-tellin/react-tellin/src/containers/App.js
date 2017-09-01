4ff3847ec639f2bcc23c2e084f1e4b301a79a280
unknown <geon0250@gmail.com>
2017-09-01 11:29:39 +0900

    Modify main page react component fixed property

--- react-tellin/react-tellin/src/containers/App.js
+++ react-tellin/react-tellin/src/containers/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Landing from './Landing';
-//import MainPage from './MainPage';
-//import Register from './Register';
+import MainPage from './MainPage';
+import Register from './Register';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Landing/>
+        <MainPage/>
       </div>
     );
   }
