506dbf82d3a96f168107a35f690bcf3483664358
PlanB <city7310@naver.com>
2017-09-04 19:18:51 +0900

    Change User schema structure, add facebook signup logics
    1. SNS 연동 회원가입 시 비워둬야 하는 password 애트리뷰트의 require를 false로
    2. createdAt -> entryDate로 이름 변경
    3. SNS 연동 시 필요한 로직을 create 함수에 if분기 형태로 추가

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -4,47 +4,65 @@ const crypto = require('crypto');
 
 const User = Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     nickname: { type: String, required: true, unique: true },
-    createdAt: { type: String, required: true },
+    entryDate: { type: String, required: true },
     signupType: { type: String, required: true },
-    last10Watched: [{ type: Schema.Types.ObjectId, ref: 'Post'}],
-    last10Liked: [{ type: Schema.Types.ObjectId, ref: 'Post'}],
-    posts: [{ type: Schema.Types.ObjectId, ref: 'Post'}],
+    last10Watched: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    last10Liked: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 }, {
-    collection: 'User'
-});
+        collection: 'User'
+    });
 
 /*
 email : 사용자 이메일 (로그인에 사용)
 password : 패스워드
 nickname : 사용자 닉네임
-createdAt : 계정 회원가입 날짜
+entryDate : 계정 회원가입 날짜
 signupType : 보류중 :: 회원가입 타입 (common, facebook)
  */
-User.statics.create = function(email, password, nickname, signupType) {
-    const secret = process.env.TELLIN_PASSWORD_SECRET;
+User.statics.create = function (email, password, nickname, signupType) {
+    if (signupType === 'common') {
+        // 일반 회원가입
+        const secret = process.env.TELLIN_PASSWORD_SECRET;
 
-    const encrypted = crypto.createHmac('sha1', secret)
-    .update(password)
-    .digest('base64');
-    
-    const date = new Date();
-    const createdAt = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
-        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        const encrypted = crypto.createHmac('sha1', secret)
+            .update(password)
+            .digest('base64');
 
-    const user = new this({
-        email,
-        "password" : encrypted,
-        nickname,
-        createdAt,
-        signupType
-    })
+        const date = new Date();
+        const entryDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
-    return user.save();
+        const user = new this({
+            email,
+            "password": encrypted,
+            nickname,
+            entryDate,
+            signupType
+        });
+
+        return user.save();
+    } else {
+        // 페이스북 연결
+        const date = new Date();
+        const entryDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        const user = new this({
+            // 짱짱 mongoose는 애트리뷰트랑 변수 이름을 알아서 매칭해서 넣어준다
+            email,
+            nickname,
+            entryDate,
+            signupType
+        });
+
+        return user.save();
+    }
 }
 
-User.statics.findOneByEmail = function(email) {
+User.statics.findOneByEmail = function (email) {
     return this.findOne({
         email
     }).exec();
@@ -62,6 +80,8 @@ User.methods.verify = function (password) {
     const encrypted = crypto.createHmac('sha1', secret)
         .update(password)
         .digest('base64');
+
     return this.password === encrypted;
 }
+
 module.exports = mongoose.model('User', User);
