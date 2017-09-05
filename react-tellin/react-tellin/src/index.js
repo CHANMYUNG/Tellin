3170f0fcc3eebb97940420c11d0a2c8f2918114b
unknown <geon0250@gmail.com>
2017-09-05 18:58:55 +0900

    Modify routing method

--- react-tellin/react-tellin/src/index.js
+++ react-tellin/react-tellin/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import Landing from './containers/Landing';
+import Register from './containers/Register';
 
+const root = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={Landing}/> 
+            <Route path = "/register" component = {Register}/>
+        </div>
+    </BrowserRouter>
+), root);
