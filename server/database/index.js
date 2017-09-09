21436aed7cb74fdceddb1eae584fdf91d2910748
JoMingyu <city7310@naver.com>
2017-09-09 13:29:46 +0900

    Change mongoose connection syntaxes
    DeprecationWarning이 뜨는 connect()를 createConnection()으로 변경

--- server/database/index.js
+++ server/database/index.js
@@ -3,12 +3,12 @@ let mongoose = require('mongoose');
 module.exports = () => {
     mongoose.Promise = global.Promise;
     // process.env는 환경변수에 접근함.
-    mongoose.connect(process.env.TELLIN_DB_URL);
-    mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error.'));
-    mongoose.connection.on('open', function () {
+    const connection = mongoose.createConnection(process.env.TELLIN_DB_URL);
+    connection.on('error', console.error.bind(console, 'mongoose connection error.'));
+    connection.on('open', function () {
         console.log(`Connected to database - ${mongoose.connection.db.databaseName}`);
     });
-    mongoose.connection.on('disconnected', () => {
+    connection.on('disconnected', () => {
         console.log(`Disconnected to database`);
     });
 };
