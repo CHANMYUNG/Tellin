6035ce178f2c2b60e20952c7f2782e6b9b4ddbe9
KoreanJungYJ <jung22337@naver.com>
2017-09-19 19:39:05 +0900

    logoPart is created

--- react-tellin/src/components/MainSideLayout.js
+++ react-tellin/src/components/MainSideLayout.js
@@ -4,7 +4,25 @@ class MainSideBarLayout extends Component{
     render(){
         return(
             <div className = "sideLayout">
+                <LogoPart logoUrl = {require('../images/RESOURCE/Dashboard_Main/ic_logo.png')}/>
+                <MainNavigation/>
+            </div>
+        );
+    }
+}
 
+const LogoPart = (props) => {
+    return(
+        <div id = "logoPart">
+            <img src = {props.logoUrl} alt = "mainLogo" id = "mainLogo"/>
+        </div>
+    );
+}
+
+class MainNavigation extends Component{
+    render(){
+        return(
+            <div>
             </div>
         );
     }
