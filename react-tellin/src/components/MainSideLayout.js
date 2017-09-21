61b21bfe4a3091d2f19c0a981829fcfd6e2a6ed5
KoreanJungYJ <jung22337@naver.com>
2017-09-21 14:22:15 +0900

    hover animations are applied

--- react-tellin/src/components/MainSideLayout.js
+++ react-tellin/src/components/MainSideLayout.js
@@ -1,6 +1,39 @@
 import React, {Component} from 'react';
 
 class MainSideBarLayout extends Component{
+    componentDidMount(){
+        let navImgs = document.getElementsByClassName('navImgs');
+        let imgs = document.getElementsByClassName('imgs');
+
+        Array.from(navImgs).forEach((elem, index) => {        
+            elem.addEventListener('mouseover', () => {
+                let hoverImgs = [
+                    require('../images/RESOURCE/Dashboard_Main/ic_home_white.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_love_white.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_group_white.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_person_white.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_doller_white.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_more_white.png')
+                ];
+                
+                
+                imgs[index].src = hoverImgs[index];
+            });
+            elem.addEventListener('mouseout', () => {
+                let hoverImgs = [
+                    require('../images/RESOURCE/Dashboard_Main/ic_home_gray.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_love_gray.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_group_gray.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_person_gray.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_doller_gray.png'),
+                    require('../images/RESOURCE/Dashboard_Main/ic_more_gray.png')
+                ];
+                
+                imgs[index].src = hoverImgs[index];
+            });
+        });
+    }
+
     render(){
         return(
             <div className = "sideLayout">
@@ -23,10 +56,78 @@ class MainNavigation extends Component{
     render(){
         return(
             <div id = "navPart">
-                
+                <UserInfo/>
+                <NavImgs navImgs = {
+                    [
+                        {
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_home_gray.png'),
+                            "ImgTitle" : "메인으로",
+                            "IdName" : "mainNav",
+                            "ImgName" : "mainImg"
+                        },
+                        {
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_love_gray.png'),
+                            "ImgTitle" : "연애소통",
+                            "IdName" : "loveNav",
+                            "ImgName" : "loveImg"
+                        },
+                        {   
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_group_gray.png'),
+                            "ImgTitle" : "가족소통",
+                            "IdName" : "FamilyNav",
+                            "ImgName" : "familyImg"
+                        },
+                        {
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_person_gray.png'),
+                            "ImgTitle" : "친구소통",
+                            "IdName" : "FriendNav",
+                            "ImgName" : "friendImg"
+                        },
+                        {
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_doller_gray.png'),
+                            "ImgTitle" : "금전소통",
+                            "IdName" : "MoneyNav",
+                            "ImgName" : "moneyImg"
+                        },
+                        {
+                            "ImgUrl" : require('../images/RESOURCE/Dashboard_Main/ic_more_gray.png'),
+                            "ImgTitle" : "기타소통",
+                            "IdName" : "EtcNav",
+                            "ImgName" : "etcImg"
+                        }
+                    ]
+                }/>
             </div>
         );
     }
+}
+
+class UserInfo extends Component{
+    render(){
+        return(
+            <div id = "userInfo">
+
+            </div>
+        );
+    }
+}
+
+const NavImgs = (props) => {
+    return(
+        <div id = "navImgsCover">
+            {props.navImgs.map((imgs, key) => {
+                return(
+                    <div className = "navImgs" key = {key}>
+                        <img src = {imgs.ImgUrl} className = "imgs" id = {imgs.ImgName}/>
+
+                        <div className = "imgTitles" id = {imgs.IdName}>
+                            {imgs.ImgTitle}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default MainSideBarLayout;
